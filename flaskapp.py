853b4284960103b5b3b3eac6d534abd0b1382581
Saafo <saffordh@163.com>
2020-05-15 09:20:59 +0800

    feat: database exception decorator

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask import request
+from psycopg2 import DataError, InternalError
+from functools import wraps
 
 from user import *
 from info import *
 from moments import *
+
+def handle_database_exception(conn):
+    def handle(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except (DataError, InternalError):
+                conn.rollback()
+                return "FUCKYOUSCRIPTBOY", 403 #尝试SQL注入时会引发异常，返回异常信息。
+        return wrapper
+    return handle
 
 def flaskapp(cur, conn):
     app = Flask(__name__)
@@ -19,109 +33,87 @@ def flaskapp(cur, conn):
             json = request.args
             print(all([json.get('arg'),json.get('tes')]))
             return json
-
+    #TODO 发生except之后把错误显示出来！！！
     @app.route('/user/register') #用户注册
+    @handle_database_exception(conn)
     def register_route():
-        try:
-            return registerlogin.registerlogin(cur, conn, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY" #尝试SQL注入时会引发异常，返回异常信息。
+        return registerlogin.registerlogin(cur, conn, request.args)
 
     @app.route('/user/login') #用户登录
+    @handle_database_exception(conn)
     def login_route():
-        try:
-            return registerlogin.registerlogin(cur, conn, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return registerlogin.registerlogin(cur, conn, request.args)
+
 
     @app.route('/user/updatetoken') #Token更新
+    @handle_database_exception(conn)
     def updatetoken_route():
-        try:
-            return updatetoken.updatetoken(cur, conn, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return updatetoken.updatetoken(cur, conn, request.args)
+
 
     @app.route('/user/getselfinfo') #获取个人信息
+    @handle_database_exception(conn)
     def getselfinfo_route():
-        try:
-            return getselfinfo.getselfinfo(cur, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return getselfinfo.getselfinfo(cur, request.args)
+
 
     @app.route('/user/getpublicinfo') #获取用户公众信息
+    @handle_database_exception(conn)
     def getpublicinfo_route():
-        try:
-            return getpublicinfo.getpublicinfo(cur, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return getpublicinfo.getpublicinfo(cur, request.args)
+
 
     @app.route('/user/updatedata', methods=["POST"]) #更新个人信息
+    @handle_database_exception(conn)
     def updatedata_route():
-        try:
-            return updatedata.updatedata(cur, conn, request.get_json())
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return updatedata.updatedata(cur, conn, request.get_json())
+
 
     @app.route('/info/getstatistic') #获取历史数据
+    @handle_database_exception(conn)
     def getstatistic_route():
-        try:
-            return getstatistic.getstatistic(cur, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return getstatistic.getstatistic(cur, request.args)
+
 
     @app.route('/info/poststatistic', methods=["POST"]) #更新数据
+    @handle_database_exception(conn)
     def poststatistic_route():
-        try:
-            return poststatistic.poststatistic(cur, conn, request.get_json())
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return poststatistic.poststatistic(cur, conn, request.get_json())
+
 
     @app.route('/moments/getmomentsid') #获取`id为xxxx用户`或者`全体用户`的所有MomentsID
+    @handle_database_exception(conn)
     def getmomentsid_route():
-        try:
-            return getmomentsid.getmomentsid(cur, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return getmomentsid.getmomentsid(cur, request.args)
+
 
     @app.route('/moments/getmoment') #获取单条动态内容
+    @handle_database_exception(conn)
     def getmoment_route():
-        try:
-            return getmoment.getmoment(cur, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return getmoment.getmoment(cur, request.args)
+
 
     @app.route('/moments/thumbup') #点赞
+    @handle_database_exception(conn)
     def thumbup_route():
-        try:
-            return thumbup.thumbup(cur, conn, request.args)
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return thumbup.thumbup(cur, conn, request.args)
+
 
     @app.route('/moments/postmoment', methods=["POST"]) #发动态
+    @handle_database_exception(conn)
     def postmoment_route():
-        try:
-            return postmoment.postmoment(cur, conn, request.get_json())
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return postmoment.postmoment(cur, conn, request.get_json())
+
 
     @app.route('/moments/postcomment', methods=["POST"]) #评论
+    @handle_database_exception(conn)
     def postcomment_route():
-        try:
-            return postcomment.postcomment(cur, conn, request.get_json())
-        except Exception:
-            conn.rollback()
-            return "FUCKYOUSCRIPTBOY"
+        return postcomment.postcomment(cur, conn, request.get_json())
+
+    
+    # @app.handle_exception()
+    # @app.errorhandler()
+    def null(): #TODO 看看怎么用上面两个东西
+        pass
 
     app.run() #TODO debugging
--- returnmsg.py
+++ returnmsg.py
@@ -5,7 +5,7 @@ def empty(msg):
     return {'errorCode':-1, 'errorMsg':msg}
 
 def error(msg):
-    return {'errorCode':-2, 'errorMsg':msg}
+    return {'errorCode':-2, 'errorMsg':msg}, 400
 
 def tokeninvalid():
-    return {'errorCode':-3, 'errorMsg':'Token过期或不合法，请重新登录'}
+    return {'errorCode':-3, 'errorMsg':'Token过期或不合法，请重新登录'}, 401
