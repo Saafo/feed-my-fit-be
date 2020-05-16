924964f59a7dab451aae1b848c29422d976b65b4
Saafo <saffordh@163.com>
2020-05-16 10:51:19 +0800

    init: thumbup finished

--- moments/thumbup.py
+++ moments/thumbup.py
@@ -1,6 +1,7 @@
 # about thumbup
 import userToken
 import returnmsg
+from psycopg2 import sql
 
 def thumbup(cur, conn, args):
     id = args.get('id')
@@ -14,6 +15,33 @@ def thumbup(cur, conn, args):
     if userToken.testToken(cur, id, token) == False:
         return returnmsg.tokeninvalid()
 
-    #TODO 数据库找到momentid, thumb+1
+    #数据库找到momentid, thumb+1
+    cur.execute(
+        sql.SQL(
+            'SELECT {Thumbs} FROM moments '
+            'WHERE {MomentID} = %s'
+        ).format(
+            Thumbs=sql.Identifier("Thumbs"),
+            MomentID=sql.Identifier("MomentID")
+        ),(momentid,)
+    )
+    row = cur.fetchone()
+    if row == None:
+        return returnmsg.error("点赞的动态不存在")
+    thumbs = row[0]
+    #点赞操作
+    thumbs += 1 
+    #写入数据库
+    cur.execute(
+        sql.SQL(
+            'UPDATE moments '
+            'SET {Thumbs} = %s '
+            'WHERE {MomentID} = %s'
+        ).format(
+            Thumbs=sql.Identifier("Thumbs"),
+            MomentID=sql.Identifier("MomentID")
+        ),(thumbs, momentid)
+    )
+    conn.commit()
 
     return returnmsg.success({})
